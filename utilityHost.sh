7a044d241e89e35aefabd33aeb03ecd8261d194d
brusmax <enrique.webgenius@gmail.com>
2023-01-20 17:01:15 -0600

    test utilityHOst homework

---
+++ utilityHost.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# Homework
+
+
+echo "Please give the value of option:"
+read option
+echo "Now the value of result:"
+read result
+echo "Your values are: \n Option: $option\nResult: $result"
+
