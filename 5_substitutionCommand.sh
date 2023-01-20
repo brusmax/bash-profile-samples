b13013969482967818ba3bc72957cad3c98c4392
brusmax <enrique.webgenius@gmail.com>
2023-01-20 15:32:46 -0600

    substitution command

---
+++ 5_substitutionCommand.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+# Script to execute commands and save vars to use with operator types 
+# Author: @brusmax - hola@brusmax.com
+
+currentLocation=`pwd`
+infoKernel=$(uname -a)
+
+echo "Current location: $currentLocation"
+echo "Kernel information: $infoKernel"
+
