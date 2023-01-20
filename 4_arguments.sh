9db3849ad9c633d1d5ff8bb4fd155960b964b973
brusmax <enrique.webgenius@gmail.com>
2023-01-20 15:19:43 -0600

    arguments

---
+++ 4_arguments.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+# Script arguments examples
+# Author: @brusmax - hola@brusmax.com
+
+curseName=$1
+schedule=$2
+
+echo "Curse name is: $curseName which is at: $schedule"
+echo "Number of params sent: $#"
+echo "Params sent: $*"
+
